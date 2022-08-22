a48aeed1215974562c44ca88a379fec9fe5c5c7a
ohubabs <obakinolugbade@gmail.com>
2022-08-22 15:06:57 -0700

    Tested and approved by client

--- LA.sh
+++ LA.sh
@@ -2,7 +2,8 @@
 echo "California knows how to party"
 echo "California knows how to party"
 echo "In the city, of LA"
-echo "In the city, of good ol' watts
+echo "In the city, of good ol' watts"
 echo "In the city, the city of compton"
 echo "We keep it rockin - x2"
 echo "So shake it shake it baby - x4"
+#Practice for modifying files
