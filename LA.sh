0be48dcc61258dd8339c4e897f025a82e39a73bb
ohubabs <obakinolugbade@gmail.com>
2022-08-22 12:59:46 -0700

    California knows how to party

---
+++ LA.sh
@@ -0,0 +1,8 @@
+#Facts about Los Angeles
+echo "California knows how to party"
+echo "California knows how to party"
+echo "In the city, of LA"
+echo "In the city, of good ol' watts
+echo "In the city, the city of compton"
+echo "We keep it rockin"
+echo "So shake it shake it baby"
